1a84c176bebeaecdbd9bb74a56ef89cb83544e71
xavierNabais <xaviernabais0@gmail.com>
2024-04-23 03:57:34 +0100

    Update Agenda Funcionario

--- frontend/src/views/painel/funcionario/agendaFuncionario.vue
+++ frontend/src/views/painel/funcionario/agendaFuncionario.vue
@@ -3,11 +3,11 @@
     <Header />
     <div class="section2">
       <div class="content2">
-        <span class="title2">EDITAR PERFIL</span>
+        <span class="title2">AGENDA</span>
       </div>
     </div>
     <div style="width: 100%; height: 1200px; background-color: white; padding-bottom: 10%;">
-      <vue-cal :events="eventList" :config="calConfig" />
+      <vue-cal :events="eventList" :config="calConfig"  />
     </div>
     <Footer />
   </div>
@@ -75,5 +75,11 @@ name: 'agendaFuncionario',
 </script>
 
 <style scoped>
-/* Estilos específicos do componente */
+
+.vuecal__event {
+  height: 54px!important;
+}
+
+
+
 </style>
