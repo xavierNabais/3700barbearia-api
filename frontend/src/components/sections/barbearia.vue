0fdcb3380042d89c01f2f85056f7600e1f63b61b
xavierNabais <xaviernabais0@gmail.com>
2024-04-27 21:53:32 +0100

    About us section css implementation

--- frontend/src/components/sections/barbearia.vue
+++ frontend/src/components/sections/barbearia.vue
@@ -9,20 +9,36 @@
             <p style="font-size: 24px;color: white;line-height: 50px">
                 Bem-vindo à Barbearia [Nome] Localizada no coração da cidade, a Barbearia [Nome] oferece serviços de barbearia de alta qualidade para homens que procuram um corte de cabelo e barba impecáveis. Com uma equipe de barbeiros experientes e apaixonados, estamos comprometidos em proporcionar uma experiência de cuidado pessoal excepcional a cada cliente.
             </p>
-
-
+            <div style="gap:30px;display: flex;margin-top:10%;">
+                <div><img src="../../assets/images/aboutus.jpg" style="width:100%;" class="lightbox"></div>
+                <div><img src="../../assets/images/aboutus.jpg" style="width:100%" class="lightbox"></div>
+                <div><img src="../../assets/images/aboutus.jpg" style="width:100%" class="lightbox"></div>
+                <div><img src="../../assets/images/aboutus.jpg" style="width:100%" class="lightbox"></div>
+            </div>
             </div>
         </div>
         </section>
 </template>
 
 <script>
-
 export default {
 name: 'SectionBarbearia',
-
- 
 
 }
 
 </script>
+
+
+<style scoped>
+
+.lightbox:hover{
+    transform: translateY(-15px);
+    transition:0.2s;
+}
+
+.lightbox{
+    transition:0.2s;
+    border: 1px solid white;
+}
+
+</style>
