983b15d51bdc316115b62447a74d0df1a8df0097
xavierNabais <xaviernabais0@gmail.com>
2024-04-27 02:31:19 +0100

    Internal Fixes & OTHERS

--- frontend/src/components/sections/barbearia.vue
+++ frontend/src/components/sections/barbearia.vue
@@ -5,22 +5,23 @@
                 <img src="../../assets/images/bigode_line.png" alt="Bigode" style="width:10%;">
             </div>
         <div class="conteudo">
-            <div class="conteudo-imagem">
-            <img src="../../assets/images/about_logo.jpg" alt="Imagem" class="imagem-conteudo">
-            </div>
             <div class="conteudo-texto">
-            <p style="font-size: 32px; color: white;">BARBEARIA 3700</p>
-            <p style="font-size: 24px;color: white;">
-                Bem-vindo à Barbearia [Nome] Localizada no coração da cidade, a Barbearia [Nome] oferece serviços de barbearia de alta qualidade para homens que procuram um corte de cabelo e barba impecáveis. Com uma equipe de barbeiros experientes e apaixonados, estamos comprometidos em proporcionar uma experiência de cuidado pessoal excepcional a cada cliente. Desde cortes clássicos até as últimas tendências em barba e cabelo, nossa barbearia é o local ideal para homens que valorizam a qualidade e o estilo. Agende uma visita e descubra o que a Barbearia [Nome] tem a oferecer! 
+            <p style="font-size: 24px;color: white;line-height: 50px">
+                Bem-vindo à Barbearia [Nome] Localizada no coração da cidade, a Barbearia [Nome] oferece serviços de barbearia de alta qualidade para homens que procuram um corte de cabelo e barba impecáveis. Com uma equipe de barbeiros experientes e apaixonados, estamos comprometidos em proporcionar uma experiência de cuidado pessoal excepcional a cada cliente.
             </p>
+
+
             </div>
         </div>
         </section>
 </template>
 
 <script>
+
 export default {
 name: 'SectionBarbearia',
+
+ 
 
 }
 
