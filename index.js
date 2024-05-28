e9744141ae2edf1512189e88b93ce835f2fe52d9
xavierNabais <xaviernabais0@gmail.com>
2024-05-28 21:34:20 +0100

    changes to index

--- index.js
+++ index.js
@@ -3,25 +3,20 @@ const session = require('express-session');
 const app = express();
 const path = require('path');
 const cors = require("cors");
-const boydParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 //Configurações
-app.set('port', process.env.port || process.env.PORT || 5000);
-app.use(express.urlencoded({extended:true}));
-app.listen(app.get('port'), () => {
-console.log('Servidor iniciado na porta: '+ app.get('port'));
-});
+app.set('port', process.env.PORT || 5000);
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(cors());
-app.use(boydParser.urlencoded({extended: false}));
-app.use(boydParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(session({
     secret: 'v19bnqYOC8',
     resave: false,
     saveUninitialized: true
-  }));
-
-
+}));
 
 //Definir Rotas Principais
 var rotas = require("./routes/main.route");
@@ -30,10 +25,15 @@ app.use("/", rotas);
 var utilizadores = require("./routes/adminPanel.route");
 app.use("/", utilizadores);
 
-
 //Definir Rotas Inexistentes
-app.use(function(req,res){
+app.use(function (req, res) {
     res.type('text/plain')
     res.status(404)
     res.send('404 not found')
-})
+});
+
+// Iniciar o servidor
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log('Servidor iniciado na porta: ' + port);
+});
