1f9c04f4d9bc148af903e824f869b60dbf100b53
xavierNabais <xaviernabais0@gmail.com>
2024-05-28 22:19:31 +0100

    try to use free db

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    db: '3700barber'
+    host: 'sql7.freemysqlhosting.net',
+    user: 'sql7710063',
+    password: 'xy5hcntqka',
+    db: 'sql7710063'
 }
