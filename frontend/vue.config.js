0b781badbbf95521aa8598c65c1fb36d57bbe110
xavierNabais <xaviernabais0@gmail.com>
2024-05-28 20:19:53 +0100

    dist fix

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
+  outputDir: 'dist', // Especifica o diretório de saída como 'dist'
   transpileDependencies: true
 })
