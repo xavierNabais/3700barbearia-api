5c0678edc0df89a8904a892e8c79e90055f2533b
xavierNabais <xaviernabais0@gmail.com>
2024-05-28 21:03:47 +0100

    Adicionando pasta 'api' do repositório '3700barberia'

--- frontend/vue.config.js
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  outputDir: 'dist', // Especifica o diretório de saída como 'dist'
-  transpileDependencies: true
-})
